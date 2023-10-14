cceca422ecda0bc152981ff0e52166a2834c2718
Michael Aquino <142678008+Mic10711@users.noreply.github.com>
2023-10-14 11:20:42 -0700

    Create index.js
    With jQuery

---
+++ index.js
@@ -0,0 +1,12 @@
+$('.btn').on('click', function() {
+    $('#correct').text('Correct!');
+ $("#correct").css("background-color", "green");
+    $('#incorrect1').text('Incorrect!');
+ $("#incorrect1").css("background-color", "red")
+   $('#incorrect2').text('Incorrect!');
+ $("#incorrect2").css("background-color", "red");
+    $('#incorrect3').text('Incorrect!');
+ $("#incorrect3").css("background-color", "red");
+});
+
+
